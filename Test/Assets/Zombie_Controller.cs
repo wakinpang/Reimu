e318380ec811724bd9474895a221550cb6681df1
wakinpang <550909227@qq.com>
2015-11-12 00:46:48 +0800

    TEST
    just a test

---
+++ Test/Assets/Zombie_Controller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zombie_Controller : MonoBehaviour {
+
+	//public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

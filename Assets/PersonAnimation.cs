a5264327003c2f4e6ecdb0525524ecd5fa201cdc
wakinpang <550909227@qq.com>
2015-11-14 16:10:32 +0800

    Get Pictures of person animation

---
+++ Assets/PersonAnimation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersonAnimation : MonoBehaviour {
+
+	public Sprite[] spritesStand, spritesWalkFront, spritesJump;
+	public float framesPerSecond;
+	private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<Renderer>() as SpriteRenderer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Fire1")) {
+			int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
+			index = index % spritesWalkFront.Length;
+			spriteRenderer.sprite = spritesWalkFront[index];
+		} else {
+			int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
+			index = index % spritesStand.Length;
+			spriteRenderer.sprite = spritesStand[index];
+		}
+	}
+}
